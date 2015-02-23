ffa5957bb75d71ec8bed3077ef403d5fcdb4ff6e
Christopher Fernández <fernandez.chl@gmail.com>
2015-02-23 10:33:00 -0300

    pequeño fix al scrapping

--- app/controllers/buy_lists_controller.rb
+++ app/controllers/buy_lists_controller.rb
@@ -85,10 +85,10 @@ class BuyListsController < ApplicationController
     cards.each do |card|
       doc = Nokogiri::HTML(open(card.link))
 
-      if !doc.at_css('table.prod-variants .even td').blank?
-        card.condition = doc.at_css('table.prod-variants .even td').text
-        card.price = doc.at_css('table.prod-variants .even td .price').text[/[0-9\.]+/]
-        card.stock = doc.at_css('table.prod-variants .even td .qty').text[/[0-9\.]+/]
+      if !doc.at_css('.clearfix table.prod-variants .even td').blank?
+        card.condition = doc.at_css('.clearfix table.prod-variants .even td').text
+        card.price = doc.at_css('.clearfix table.prod-variants .even td .price').text[/[0-9\.]+/]
+        card.stock = doc.at_css('.clearfix table.prod-variants .even td .qty').text[/[0-9\.]+/]
       else
         card.stock = nil
         card.price = 0
